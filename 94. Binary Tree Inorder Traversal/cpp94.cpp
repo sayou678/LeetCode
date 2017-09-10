e905000904860ecc855598c363c3a02e4972ca03
Damon Li <sayou678@126.com>
2017-09-10 20:51:37 +0800

    1

--- 94. Binary Tree Inorder Traversal/cpp94.cpp
+++ 94. Binary Tree Inorder Traversal/cpp94.cpp
@@ -2,96 +2,96 @@
 
 
 struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-	vector<int> inorderTraversal(TreeNode* root) 
-	{
-		stack<TreeNode *> s;
-		TreeNode *node = root;
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        stack<TreeNode *> s;
+        TreeNode *node = root;
 
-		vector<int> ret;
+        vector<int> ret;
 
-		while(!s.empty() || node != NULL)
-		{
-			if(node != NULL)
-			{
-				s.push(node);
-				node = node->left;
-			}
-			else
-			{				
-				node = s.top();
-				s.pop();
-				ret.push_back(node->val);					
-				node = node->right;			              
-			}
-		}
+        while(!s.empty() || node != NULL)
+        {
+            if(node != NULL)
+            {
+                s.push(node);
+                node = node->left;
+            }
+            else
+            {               
+                node = s.top();
+                s.pop();
+                ret.push_back(node->val);                   
+                node = node->right;                       
+            }
+        }
 
-		return ret;
-	}
+        return ret;
+    }
 
-	vector<int> preorderTraversal(TreeNode* root) 
-	{
-		stack<TreeNode *> s;
-		TreeNode *node = root;
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+        stack<TreeNode *> s;
+        TreeNode *node = root;
 
-		vector<int> ret;
-		while(!s.empty() || node != NULL)
-		{
-			if(node != NULL)
-			{
-				s.push(node);
-				ret.push_back(node->val);	
-				node = node->left;
-			}
-			else
-			{				
-				node = s.top();
-				s.pop();									
-				node = node->right;			              
-			}
-		}
-		return ret;
-	}
+        vector<int> ret;
+        while(!s.empty() || node != NULL)
+        {
+            if(node != NULL)
+            {
+                s.push(node);
+                ret.push_back(node->val);   
+                node = node->left;
+            }
+            else
+            {               
+                node = s.top();
+                s.pop();                                    
+                node = node->right;                       
+            }
+        }
+        return ret;
+    }
 
-	vector<int> postorderTraversal(TreeNode* root) 
-	{
-		stack<TreeNode *> s;
-		TreeNode *node = root;
-		TreeNode *lastNodeVisited = NULL;
-		TreeNode *peekNode = NULL;
+    vector<int> postorderTraversal(TreeNode* root) 
+    {
+        stack<TreeNode *> s;
+        TreeNode *node = root;
+        TreeNode *lastNodeVisited = NULL;
+        TreeNode *peekNode = NULL;
 
-		vector<int> ret;
-		while(!s.empty() || node != NULL)
-		{
-			if(node != NULL)
-			{
-				s.push(node);				
-				node = node->left;
-			}
-			else
-			{				
-				peekNode = s.top();
-				if (peekNode->right != NULL && peekNode->right != lastNodeVisited)
-				{
-					node = peekNode->right;
-				}
-				else
-				{	
-					ret.push_back(peekNode->val);
-					lastNodeVisited = peekNode;
-					s.pop();
-				}               
-			}
-		}
-		return ret;
-	}
+        vector<int> ret;
+        while(!s.empty() || node != NULL)
+        {
+            if(node != NULL)
+            {
+                s.push(node);               
+                node = node->left;
+            }
+            else
+            {               
+                peekNode = s.top();
+                if (peekNode->right != NULL && peekNode->right != lastNodeVisited)
+                {
+                    node = peekNode->right;
+                }
+                else
+                {   
+                    ret.push_back(peekNode->val);
+                    lastNodeVisited = peekNode;
+                    s.pop();
+                }               
+            }
+        }
+        return ret;
+    }
 
 };
 
@@ -99,63 +99,63 @@ void trimLeftTrailingSpaces(string &input) {
 }
 
 TreeNode* stringToTreeNode(string input) {
-	input = input.substr(1, input.length() - 2);
-	if (!input.size()) {
-		return NULL;
-	}
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return NULL;
+    }
 
-	string item;
-	stringstream ss;
-	ss.str(input);
+    string item;
+    stringstream ss;
+    ss.str(input);
 
-	getline(ss, item, ',');
-	TreeNode* root = new TreeNode(atoi(item.c_str()));
-	queue<TreeNode*> nodeQueue;
-	nodeQueue.push(root);
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(atoi(item.c_str()));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
 
-	while (true) {
-		TreeNode* node = nodeQueue.front();
-		nodeQueue.pop();
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
 
-		if (!getline(ss, item, ',')) {
-			break;
-		}
+        if (!getline(ss, item, ',')) {
+            break;
+        }
 
-		trimLeftTrailingSpaces(item);
-		if (item != "null") {
-			int leftNumber = atoi(item.c_str());
-			node->left = new TreeNode(leftNumber);
-			nodeQueue.push(node->left);
-		}
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = atoi(item.c_str());
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
 
-		if (!getline(ss, item, ',')) {
-			break;
-		}
+        if (!getline(ss, item, ',')) {
+            break;
+        }
 
-		trimLeftTrailingSpaces(item);
-		if (item != "null") {
-			int rightNumber = atoi(item.c_str());
-			node->right = new TreeNode(rightNumber);
-			nodeQueue.push(node->right);
-		}
-	}
-	return root;
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = atoi(item.c_str());
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
 }
 
 string integerVectorToString(vector<int> list, int length = -1) {
-	if (length == -1) {
-		length = list.size();
-	}
+    if (length == -1) {
+        length = list.size();
+    }
 
-	string result;
-	for(int index = 0; index < length; index++) {
-		int number = list[index];
-		stringstream ss;
-		ss << number;
-		string str = ss.str();
-		result += str + ", ";
-	}
-	return result.substr(0, result.length() - 2);
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        stringstream ss;
+        ss << number;
+        string str = ss.str();
+        result += str + ", ";
+    }
+    return result.substr(0, result.length() - 2);
 }
 
 
--- 94. Binary Tree Inorder Traversal/main.cpp
+++ 94. Binary Tree Inorder Traversal/main.cpp
@@ -16,22 +16,22 @@ using namespace std;
 
 int main() {
     string line = "[1,2,4,8,null,null,null,5,null,9,3,6,null,10,7,null]";
-	//string line = "[1,null,2,3]";
+    //string line = "[1,null,2,3]";
 
 
-	TreeNode* root = stringToTreeNode(line);
+    TreeNode* root = stringToTreeNode(line);
 
-	vector<int> ret = Solution().inorderTraversal(root);
-	string out = integerVectorToString(ret);
-	cout << out << endl;	
+    vector<int> ret = Solution().inorderTraversal(root);
+    string out = integerVectorToString(ret);
+    cout << out << endl;    
 
-	ret = Solution().preorderTraversal(root);
-	out = integerVectorToString(ret);
-	cout << out << endl;	
+    ret = Solution().preorderTraversal(root);
+    out = integerVectorToString(ret);
+    cout << out << endl;    
 
-	ret = Solution().postorderTraversal(root);
-	out = integerVectorToString(ret);
-	cout << out << endl;	
-	
-	return 0;
+    ret = Solution().postorderTraversal(root);
+    out = integerVectorToString(ret);
+    cout << out << endl;    
+    
+    return 0;
 }
