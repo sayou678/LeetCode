38ab28b2e3ad5b9d9fc2237d8e5ec2391e36a470
Damon Li <sayou678@126.com>
2017-09-15 21:13:21 +0800

    add c list

--- test1/test1/test1.cpp
+++ test1/test1/test1.cpp
@@ -14,6 +14,8 @@
 #include <locale>
 #include <queue>
 #include <sstream>
+#include <list>
+#include <assert.h>
 using namespace std;
  
 
@@ -38,14 +40,160 @@ void test94()
 	ret = Solution().postorderTraversal(root);
 	out = integerVectorToString(ret);
 	cout << out << endl;	
+}
 
-	
+typedef int BOOL;
+
+typedef struct sNode
+{
+	int val;
+	struct sNode *next;
+	struct sNode *pre;
+}Node;
+
+typedef struct sList
+{
+	Node *head;
+	unsigned size;
+}List;
+
+List * CreateList()
+{
+	List *list = (List *)malloc(sizeof(List));
+	assert(list != NUll);
+
+	list->head = NULL;
+	list->size = 0;
+
+	return list;
+}
+
+void PushBack(List *list, int val)
+{
+	if (list == NULL)
+	{
+		return;
+	}
+
+	Node *node = (Node *)malloc(sizeof(Node));
+	if (node == NULL)
+	{
+		return;
+	}
+	node->val = val;
+
+	if (list->head == NULL)
+	{
+		list->head = node;
+		list->head->next = node;
+		list->head->pre = node;
+	}
+	else
+	{
+		list->head->pre->next = node;
+		node->next = list->head;
+		node->pre = list->head->pre;
+		list->head->pre = node;
+	}
+
+	list->size++;
+}
+
+void PushFront(List *list, int val)
+{
+	if (list == NULL)
+	{ 
+		return;
+	}
+
+	Node *node = (Node *)malloc(sizeof(Node));
+	if (node == NULL)
+	{
+		return;
+	}
+	node->val = val;
+
+	if (list->head == NULL)
+	{
+		list->head = node;
+		list->head->next = node;
+		list->head->pre = node;
+	}
+	else
+	{
+		list->head->pre->next = node;
+		node->next = list->head;
+		node->pre = list->head->pre;
+		list->head->pre = node;
+
+		list->head = node;
+	}
+
+	list->size++;
+}
+
+int Back(List *list)
+{
+	assert(list != NULL && list->head != NULL);
+	return list->head->pre->val;
+}
+
+int Front(List *list)
+{
+	assert(list != NULL && list->head != NULL);
+	return list->head->val;
+}
+
+void PopBack(List *list)
+{
+
+}
+
+void PopFront(List *list)
+{
+
+}
+
+void DestroyList(List *list)
+{
+
+}
+
+void Dump(List *list)
+{
+	assert(list != NULL);
+
+	if (list->size == 0)
+	{
+		return;
+	}
+
+	Node *p = list->head;
+	do
+	{
+		cout << p->val << ", ";
+		p = p->next;
+	}
+	while (p != list->head);
+}
+
+
+void test1()
+{
+	List *list = CreateList();
+	PushBack(list, 1);
+	PushBack(list, 3);
+	PushBack(list, 4);
+	PushFront(list, 2);
+	PushFront(list, 5);
+
+	Dump(list);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	
-   test94();
+   test1();
 
 	return 0;
 }
