06641766a5d73bb52821b31805e2f224902054b1
Damon Li <sayou678@126.com>
2017-09-28 14:28:21 +0800

    u

--- C_test/sll.c
+++ C_test/sll.c
@@ -30,7 +30,22 @@ void destroyList()
 
 void addList(list *pList, char *str)
 {
+	node *n = NULL;
+	assert(pList != NULL);
 
+	n = (node *)malloc(sizeof(node));
+	assert(n != NULL);
+
+	if (pList->head == NULL)
+	{
+		pList->head = n;
+		n->next = NULL;
+	}
+	else
+	{
+		n->next = pList->head;
+		pList->head = n;
+	}
 }
 
 int compare(char *str1, char *str2)
@@ -45,21 +60,35 @@ void sort(list *pList)
 
 void dumpList(list *pList)
 {
+	node *p = NULL;
+	if (pList == NULL && pList->head == NULL)
+	{
+		return;
+	}
 
-
+	p = pList->head;
+	
+	while(p != NULL)
+	{
+		printf("%s\n", p->str);
+		p = p->next;
+	}
 }
 
 void test99()
 {
 	char *strList[] = {"Hello world", "apple", "food", "car", "house", "foot", "cup"};
 
+	int i = 0;
 	list *lst = createList();
-	assert(0);
+	assert(lst);
 
-	printf("hello\n");
+	for (i = 0; i < sizeof(strList); i++)
+	{
+		addList(lst, strList[i]);
+	}
+
+	dumpList(lst);
 
     printf("%s", "Hello world");
-
-
-
 }
